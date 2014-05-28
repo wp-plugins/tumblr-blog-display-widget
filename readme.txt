fb6241e2bbb896aad9cb97a19814be0ad7352301
matt.king <matt.king@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-28 07:08:05 +0000

    intial version release
    git-svn-id: https://plugins.svn.wordpress.org/tumblr-blog-display-widget/trunk@922232 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,70 @@
+=== Tumblr Blog Display Widget ===
+
+Contributors: matt.king
+Tags: tumblr, tumblr blogs, tumblr blog display, tumblr blog list, tumblr blog display plugin, tumblr for wordpress, tumblr widget plugin, tumblr widget, tumblr plugin, wordpress widget
+Requires at least: 3.0.1
+Tested up to: 3.9.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Tumblr is a popular free blogging platform. Using tumblr you can post anywhere of your blogs. Use tumblr to showcase your ideas.
+
+== Description ==
+
+Tumblr is a popular free blogging platform. Using tumblr you can post anywhere of your blogs. Use tumblr to showcase your ideas.
+Tumblr is a micro blogging platform. Tumblr is also a social networking platform. Using Tumblr Blog Display Shortcode you will able to display the list of latest tumblr blogs on your website. You can put the shortcode anywhere in your wordpress theme position. Using Tumblr Blog Display Shortcode is quite easy but you must have to create Tumblr API on Tumblr Official Website.
+Tumblr Blog Display Widget – So everyone your ideas and what you’ve posted on your tumblr blog site. Tumblr Blog Display Shortcode is very useful for displaying latest blogs from tumblr in your website. And what I might haven’t mentioned yet – Tumblr Blogs will be link as well. Tumblr care about your privacy so do we. You will able to show your tumblr blog post on your website only. No mercy with your tumblr blogs privacy.
+
+By Using our Tumblr Blog Display, you will able to show latest blogs of tumblr on your wordpress websites
+
+Tumblr Blog Display is compatible with latest Wordpress Versions and Responsive.
+
+We're active for any support issues. So hope you will love it.
+
+== Installation ==
+
+Installing this plugin is same like other wordpress plugins
+
+1. Upload `tumblr-blog-display-widget` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place the widget on Widget area of wordpress by dragging and drop the item.
+
+You can install the zip file as well
+
+Configurations:
+
+Remember - Blog URL and API Key are require field. You can't display the tumblr blogs without them.
+
+Blog URL: Absolute URL of your Tumblr Blog
+
+API Key: To obtain a tumblr api key
+
+http://www.tumblr.com/oauth/apps ->> go here. Register Application - Use oAuth Consumer Key as your API key
+
+Width: Width of your Module in pixel.
+example: 400
+
+Post Limit: How many blog post you want to show.
+example: 6
+
+Show Read More : true or false
+"true" is default
+"false" for not showing read more link
+
+More info -> http://ramit-designs.com/wordpress/
+
+== Frequently Asked Questions ==
+
+Is it free?
+Yap it is :)
+
+
+== Screenshots ==
+
+Demo available here - http://ramit-designs.com/wordpress/
+1. screenshot of the frontend.
+
+== Changelog ==
+
+= 1.0 =
+stable version release
---
+++ tumblr-blog-display-widget.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * @package tumblr-blog-display-widget
+*/
+/*
+Plugin Name: Tumblr Blog Display Widget
+Plugin URI: http://www.ramit-designs.com
+Description: Display your tumblr blogs in Wordpress Widget
+Version: 1.0
+Author: Matt Armstrong
+Author URI: http://www.ramit-designs.com
+*/
+
+class TumblrBlogDisplayWidget extends WP_Widget{
+    public function __construct() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles_tumblr_blog_display_widget' ) );
+        $params = array(
+            'description' => 'Display your tumblr blogs in Wordpress Widget',
+            'name' => 'Tumblr Blog Display Widget'
+        );
+        parent::__construct('TumblrBlogDisplayWidget','',$params);
+    }
+    function register_plugin_styles_tumblr_blog_display_widget() {
+        wp_register_style( 'tumblr_blog_display_widget', plugins_url( 'style.css' , __FILE__ ) );
+        wp_enqueue_style('tumblr_blog_display_widget');
+ }
+    public function form($instance) {
+        extract($instance);
+        ?>
+<p>
+    <label for="<?php echo $this->get_field_id('title');?>">Title : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('title');?>"
+	name="<?php echo $this->get_field_name('title');?>"
+        value="<?php echo !empty($title) ? $title : "Tumblr Blog Display Widget"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('blog_url');?>">Blog URL: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('blog_url');?>"
+	name="<?php echo $this->get_field_name('blog_url');?>"
+    value="<?php echo !empty($blog_url) ? $blog_url : ""; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('api_key');?>">API Key: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('api_key');?>"
+	name="<?php echo $this->get_field_name('api_key');?>"
+    value="<?php echo !empty($api_key) ? $api_key : ""; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('width');?>">Width: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('width');?>"
+	name="<?php echo $this->get_field_name('width');?>"
+    value="<?php echo !empty($width) ? $width : "450"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('limit');?>">Limit: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('limit');?>"
+	name="<?php echo $this->get_field_name('limit');?>"
+    value="<?php echo !empty($limit) ? $limit : "6"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('text_limit');?>">Text Limit: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('text_limit');?>"
+	name="<?php echo $this->get_field_name('text_limit');?>"
+    value="<?php echo !empty($text_limit) ? $text_limit : "5"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'read_more' ); ?>">Read More: </label> 
+    <select id="<?php echo $this->get_field_id( 'read_more' ); ?>"
+        name="<?php echo $this->get_field_name( 'read_more' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="true" <?php if ($read_more == 'true') echo 'selected="true"'; ?> >True</option>
+            <option value="false" <?php if ($read_more == 'false') echo 'selected="false"'; ?> >False</option>
+    </select>
+</p>
+<?php
+    }
+    public function widget($args, $instance) {
+        extract($args);
+        extract($instance);
+        $title = apply_filters('widget_title', $title);
+        $description = apply_filters('widget_description', $description);
+		if(empty($title)) $title = "Dribbble Portfolio Shots Widget";
+        if(empty($blog_url)) $blog_url = "";
+        if(empty($limit)) $limit = "6";
+        if(empty($text_limit)) $text_limit = "5";
+        if(empty($read_more)) $read_more = "true";
+        
+    $data = "";
+    if($blog_url == '' && $api_key == ''){
+        $data .= "blog_url &amp; api_key are require field to display tumblr blog in wordpress";
+    }
+    else{
+        if(ini_get('allow_url_fopen') && function_exists('openssl_open')){
+		
+    $tumblrFeed = "http://api.tumblr.com/v2/blog/$blog_url/posts/text?api_key=$api_key&notes_info=true";
+    $tumblrFeedGrab = json_decode(file_get_contents($tumblrFeed),true);
+    $postItem[] = $tumblrFeedGrab['response']['posts'];
+    $data .= "
+        <div class='tumblr_blog_display_widget' style='width: $width";
+    $data .= "px;'>
+            <div class='blogWarp'>
+";
+    for($i=0;$i<$limit;$i++){
+        foreach($postItem as $value){
+            $data .= "<div class='postWarp'>";
+                $data .= "<div class='tumblrTitle'>";
+                    $t_post_url = $value[$i]['post_url'];
+                    $t_post_title = $value[$i]['title'];
+                    $data .= "<a href='$t_post_url' target='_blank'>$t_post_title</a>";
+                $data .= "</div>";
+                $data .= "<div class='tumblrDescription'>";
+                    $t_post_desc = $this->trimWords(strip_tags($value[$i]['body']),$text_limit);
+                    $data .= "$t_post_desc";
+                    if($read_more == "true"){
+                        $data .= "<div class='display:block'>";
+                            $data .= "<a href='$t_post_url' target='_blank'>read more..</a>";
+                        $data .= "</div>";
+                    }
+                $data .= "</div>";
+            $data .= "</div>";
+
+        }
+    }
+    $data .= "</div>
+        </div>";
+            
+        }else{
+		$data .= "Check your php.ini and make sure allow_url_fopen & openssl is set to on";
+    }
+        }
+    
+        echo $before_widget;
+        echo $before_title . $title . $after_title;
+            echo $data;
+        echo $after_widget;
+    }
+    public function trimWords($string, $limit)
+{
+    $words = explode(' ', $string);
+    return implode(' ', array_slice($words, 0, $limit));
+}
+}
+//start registering the extension
+add_action('widgets_init','register_TumblrBlogDisplayWidget');
+function register_TumblrBlogDisplayWidget(){
+    register_widget('TumblrBlogDisplayWidget');
+}
