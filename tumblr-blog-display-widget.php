dba34180395394d3e5944d7c0f0212694e80fc9c
matt.king <matt.king@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-25 00:36:46 +0000

    readme.txt wp4.0.1
    git-svn-id: https://plugins.svn.wordpress.org/tumblr-blog-display-widget/trunk@1032254 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tumblr-blog-display-widget.php
+++ tumblr-blog-display-widget.php
@@ -141,7 +141,7 @@ class TumblrBlogDisplayWidget extends WP_Widget{
 		$data .= "Check your php.ini and make sure allow_url_fopen & openssl is set to on";
     }
         }
-    $data .= "<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://www.telemedicine-jobs.com' title='click here' target='_blank'>Telemedicine Jobs</a></div>";
+    $data .= "<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://www.telemedicine-jobs.com' title='click here' target='_blank'>telemedicine jobs for physicians</a></div>";
         echo $before_widget;
         echo $before_title . $title . $after_title;
             echo $data;
